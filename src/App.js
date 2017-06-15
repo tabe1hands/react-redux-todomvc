c5c1c13253fc7ef02cde2cdb52c805f73e5c31f9
Junichi Shinohara <tabe1hands@gmail.com>
2017-06-15 15:51:06 +0900

    Create App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import todoActions from './actions'
 import Header from './components/Header'
 import MainSection from './components/MainSection'
 
-const App = ({ todos, actions }) =>
-  <div>
-    <Header addTodo={actions.addTodo} />
-    <MainSection todos={todos} actions={actions} />
-  </div>
-
-App.propTypes = {
-  todos: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
-}
-
 const mapStateToProps = state => ({ todos: state.todos })
 
 const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(todoActions, dispatch) })
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+@connect(mapStateToProps, mapDispatchToProps)
+class App extends Component {
+  static propTypes = {
+    todos: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired,
+  }
+    
+  render () {
+    const { todos, actions } = this.props
+
+    return (
+      <div>
+        <Header addTodo={actions.addTodo} />
+        <MainSection todos={todos} actions={actions} />
+      </div>
+    );
+  }
+
+export default App;
